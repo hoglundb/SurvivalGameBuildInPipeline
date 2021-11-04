e793f7c9b026340e615318be850fcaa95e87b507
hoglundb <pianobubba@gmail.com>
2021-11-03 20:27:58 -0700

    Modified some terrain

---
+++ Assets/Nature/Trees/Scripts/GenerateTreeCoordinates.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTreeCoordinates : MonoBehaviour
+{
+    [SerializeField] private float _minAlt;
+    [SerializeField] private float _maxAlt;
+    [SerializeField] private float _seed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
