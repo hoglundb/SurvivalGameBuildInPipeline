af59c7a9bb58d1b3df99ec90518824f64d5d4391
hoglundb <hoglundb@oregonstate.edu>
2022-01-24 21:29:25 -0800

    crafting menu shows ingredients with item selected

--- Assets/Scripts/Inventory/CraftableItemController.cs
+++ Assets/Scripts/Inventory/CraftableItemController.cs
@@ -7,6 +7,9 @@ using UnityEngine.UI;
 //This component is on each crafting UI panel game object. 
 public class CraftableItemController : MonoBehaviour
 {
+    [Header("Reference to the parent panel")]
+    [SerializeField] private GameObject _playerCraftingPanel;
+
     [Header("The name and description item")]
     [SerializeField] public string itemName;
     [SerializeField] [Multiline] public string itemDescription;
@@ -15,22 +18,40 @@ public class CraftableItemController : MonoBehaviour
     [SerializeField] public GameObject _craftablePrefab;
 
     [SerializeField] private GameObject _craftBtnTransform;
+    [SerializeField] private GameObject _selectItemBtnTransform;
 
     [Header("List of item/quanity ingredients")]
     [SerializeField] public List<CraftingIngredient> ingredients;
 
- 
+    private GameObject _playerCraftingDescriptionPanel;
 
     private void Awake()
     {
         //Init the event handler for the craft button
         _craftBtnTransform.GetComponent<Button>().onClick.AddListener(_OnCraftingBtnClick);
+        _selectItemBtnTransform.GetComponent<Button>().onClick.AddListener(_OnSelectItemBtnClick);
+
+        _playerCraftingDescriptionPanel = GameObject.Find("CraftingDescriptionPanel");
+        _playerCraftingDescriptionPanel.GetComponent<RectTransform>().localScale = new Vector3(0, 1, 1);
     }
 
 
     private void _OnCraftingBtnClick()
     {
-        Debug.LogError("clicked me");
+
+      
+    }
+
+
+    private void _OnSelectItemBtnClick()
+    {
+        _playerCraftingDescriptionPanel.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+        string ingredientsList = "";
+        foreach (var item in ingredients)
+        {
+            ingredientsList += item.quantity.ToString() + " " + item.ingredient + "\n";
+        }
+        _playerCraftingPanel.GetComponent<Inventory.CraftingManager>().UpdateItemIngredientsTextbox(ingredientsList);
     }
 
 
--- Assets/Scripts/Inventory/CraftingManager.cs
+++ Assets/Scripts/Inventory/CraftingManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 
 namespace Inventory
 {
@@ -9,8 +9,14 @@ namespace Inventory
     {
         [SerializeField] private GameObject _craftingItemUIPrefab;
         [SerializeField] private GameObject _itemContentUIPanel;
+        [SerializeField] private GameObject _itemIngredientsTextbox;
         [SerializeField] private float _heightPerItem = 2f;
 
+
+        public void UpdateItemIngredientsTextbox(string ingredientsList)
+        {
+            _itemIngredientsTextbox.GetComponent<Text>().text = ingredientsList;
+        }
 
         //TODO
         public void ToggleVisibility()
